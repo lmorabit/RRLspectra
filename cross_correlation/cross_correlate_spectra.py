912ac98b1d5be6167fb86e6429ab9f08f7f64c52
Leah <lmorabit@gmail.com>
2018-06-22 09:40:09 +0100

    added plot to script

--- cross_correlation/cross_correlate_spectra.py
+++ cross_correlation/cross_correlate_spectra.py
@@ -4,44 +4,42 @@ import pandas
 import matplotlib.pyplot as plt
 import time
 from scipy.interpolate import interp1d
+import viridis
 
 def find_nearest(array, value):
 	idx = (np.abs(array - value)).argmin()
 	return idx
 
-def find_array(myArray, refArray):
-	out1 = np.empty(myArray.size, dtype=int)
-	for i, value in enumerate(myArray):
-        	index = find_nearest(refArray, value)
-		out1[i] = index
-	return out1
-
 def templatespec(fmin, fmax, c_freq, vwidth):
 	speedoflight = 2.99792458e5
 
+	## find the CRRL frequencies which are in the desired frequency range
 	cfreqs = [i for i in c_freq if i >= fmin and i <=fmax]
 	cfreqs = np.asarray(cfreqs[::-1])
+	## calculate the gaussian width
 	gwidth = cfreqs * vwidth / speedoflight
+	## and transform to a gaussian sigma
 	gsig = gwidth / 2.0 / np.sqrt(2.0* np.log(2.0))
 
+	## define a continuum
 	nabs = (fmax -fmin) * 1e5
 	cont_abscissa = np.arange(nabs)/1e5 + fmin
 	cont_values = np.zeros(cont_abscissa.size)
 
-	#print('cont_abscissa',cont_abscissa.size)
-	cfreqs_index = find_array(cfreqs,cont_abscissa)
-
-	#print('cfreqs_index',cfreqs_index)
-
+	## define values to help define gaussians
 	nvals = 10000
 	xvals = np.arange(nvals) / 1e5 - nvals/ 2 / 1e5
 	maxval = 1e0
 
-	for ii in range(0,len(cfreqs_index)-1):
-
+	## loop over cfreqs
+	for ii in range(0,len(cfreqs-1)):
+		## get the gaussian sigma for the line
 		gs = gsig[ii]
+		## calculate a temporary gaussian
 		tmpg = maxval*np.exp(-(xvals-0.0)**2/gs**2)
-	        cont_values[cfreqs_index[ii] - int((nvals / 2)):cfreqs_index[ii] + int((nvals / 2))] = cont_values[cfreqs_index[ii] - int((nvals / 2)):cfreqs_index[ii] + int((nvals / 2))] - tmpg
+		## add to continuum values in the right place
+		tmpg_index = find_nearest( cont_abscissa, cfreqs[ii] )
+	        cont_values[tmpg_index - int((nvals / 2)):tmpg_index + int((nvals / 2))] = cont_values[tmpg_index - int((nvals / 2)):tmpg_index + int((nvals / 2))] - tmpg
 
 	tempspec = np.zeros((2, cont_abscissa.size))
 	tempspec[0, :] = cont_abscissa
@@ -81,7 +79,10 @@ def main( spectral_file, vel_min=0., vel_max=0., vel_step=0., zmin=0., zmax=0., 
 	for ii in np.arange(0,len(vel_widths)):
 		print( 'vel width', vel_widths[ii] )
 		## get the template spectrum for the velocity width
+		t1=time.time()
 		template_spectrum = templatespec( fmin, fmax*(1.+max(redshifts)), crrl_freqs, vel_widths[ii] ) #TA, redshifting the template takes it out of range
+		t2=time.time()
+		print( 'template spectrum took ',t2-t1,' s' )
 		template_freq = template_spectrum[0,:]
 		template_opt_depth = template_spectrum[1,:]
 
@@ -96,13 +97,6 @@ def main( spectral_file, vel_min=0., vel_max=0., vel_step=0., zmin=0., zmax=0., 
 			interp_func = interp1d( template_freq_obs, template_opt_depth, kind='nearest', bounds_error=False, fill_value='extrapolate' )
 			sampled_template_opt_depth = interp_func( obs_freq )
 
-			#print('indexing...')
-			#t1=time.time()
-			#index = find_array( obs_freq, template_freq_obs )
-			#sampled_template_opt_depth = template_opt_depth[index]
-			#t2=time.time()
-			#print('finished indexing, time='t2-t1,' s')
-
 			## drop all the nans
 			finite_index = np.where( np.isfinite( obs_opt_depth ) )
 
@@ -112,8 +106,6 @@ def main( spectral_file, vel_min=0., vel_max=0., vel_step=0., zmin=0., zmax=0., 
 			## cross-correlate
 			print('running cross-correlation...')
 			t3=time.time()
-			print( samp_template_opt_depth[1:20] )
-			print( observed_opt_depth[1:20] )
 			cross_corr_coeffs[ii,jj] = np.corrcoef( samp_template_opt_depth, observed_opt_depth )[1,0]
 			t4=time.time()
 			print('finished cross-correlation! time=',t4-t3,' s')
@@ -121,6 +113,16 @@ def main( spectral_file, vel_min=0., vel_max=0., vel_step=0., zmin=0., zmax=0., 
 	outfile = spectral_file.replace('.txt','_xcorr.txt')
 	df = pandas.DataFrame(cross_corr_coeffs, columns=redshifts, index=vel_widths)
 	df.to_csv( outfile )
+
+	## make a plot
+	df = df.T
+	plt.figure()
+	df.plot(colormap='viridis')
+	plt.hlines([0.],plt.xlim()[0],plt.xlim()[1],colors='gray')
+	plt.xlabel('redshift')
+	plt.ylabel('cross-correlation coefficient')
+	plt.savefig(outfile.replace('txt','png'))
+	
 
 
 if __name__ == "__main__":
